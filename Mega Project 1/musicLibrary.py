6a7bb46cdc31f8ae5d5775508f0bbf6cc305c286
Amitava Mitra <156047274+Amitava03@users.noreply.github.com>
2026-01-14 11:15:51 +0530

    Delete Mega Project 1 directory

--- Mega Project 1/musicLibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    "stealth": "https://www.youtube.com/watch?v=1234567890",
-    "march": "https://www.youtube.com/watch?v=dDiMaUFBvq4",
-    "skyfall": "https://www.youtube.com/watch?v=sZrTJesvJeo",
-    "wolf": "https://www.youtube.com/watch?v=6677889900"
-}
